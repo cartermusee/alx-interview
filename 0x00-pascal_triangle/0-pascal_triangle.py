5efcfd33774e36330c8cb9b039d249605f744533
cartermusee <cartermusee@gmail.com>
2024-01-04 23:06:42 +0300

    python pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+# module for creating pascal triangle
+def pascal_triangle(n):
+    """
+    function itself
+    args:
+        n: the number
+    Return: triangle or 0
+    """
+    
+    if n <= 0:
+        return 0
+    else:
+        rectangle = []
+        for i in range(n):
+            row = [1] * (1 + i)
+            for j in range(1, i):
+                row[j] = rectangle[i-1][j-1] + rectangle[i-1][j]
+            rectangle.append(row)
+        return rectangle
+
