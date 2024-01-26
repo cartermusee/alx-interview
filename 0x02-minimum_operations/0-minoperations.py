23b1738e2d55d7f452b8b2f4e337349e9e538684
cartermusee <cartermusee@gmail.com>
2024-01-26 10:41:21 +0300

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""module for min operations"""
+
+
+def minOperations(n):
+    """takes a number n
+    
+    Keyword arguments:
+    n: the number
+    Return: operations
+    """
+    if n == 1:
+        return 0
+
+    def smallest_prime_factor(num):
+        """small prime num
+        
+        Keyword arguments:
+        num: the number
+        Return: num
+        """
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return i
+        return num
+
+    operations = 0
+    divisor = smallest_prime_factor(n)
+
+    while n > 1:
+        if divisor == n:
+            operations += n
+            break
+
+        while n % divisor == 0:
+            n //= divisor
+            operations += divisor
+
+        divisor = smallest_prime_factor(n)
+
+    return operations
