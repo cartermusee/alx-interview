f712cd009a1628cc044c03ff20abde03052397d9
cartermusee <cartermusee@gmail.com>
2024-02-02 10:01:12 +0300

    uf8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""moduel for utf validation"""
+
+
+def validUTF8(data):
+    """validating utf8
+    Keyword arguments:
+    data: data to avlidate
+    Return: bytes
+    """
+    bts = 0
+    for byte in data:
+        if bts:
+            if byte >> 6 == 0b10:
+                bts -= 1
+            else:
+                return False
+        else:
+            if byte >> 7 == 0:
+                bts = 0
+            elif byte >> 5 == 0b110:
+                bts = 1
+            elif byte >> 4 == 0b1110:
+                bts = 2
+            elif byte >> 3 == 0b11110:
+                bts = 3
+            else:
+                return False
+    return bts == 0
