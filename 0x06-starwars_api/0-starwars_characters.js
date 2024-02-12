384a8d11da7f6fb227ddeba90424ba3afa09636f
macilah-carter <museemacilah@gmail.com>
2024-02-12 10:36:43 +0300

    api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+const request = require('request');
+let movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url,( err, response, body) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    if (response.statusCode != 200) {
+        return;
+    }
+    const FilmData = JSON.parse(body);
+    for (let FilmChar of FilmData.characters) {
+        request(FilmChar, (err, res, body) => {
+            if (err){
+                console.log(err);
+                return;
+            }
+            if (res.statusCode != 200) {
+                return;
+            }
+            const names = JSON.parse(body);
+            console.log(names.name);   
+        })
+    }
+})
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,1 @@
+api in js suing require module
