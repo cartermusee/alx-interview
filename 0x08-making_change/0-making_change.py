731022451ed1c197fb197e0182e9be23a691cc35
macilah-carter <museemacilah@gmail.com>
2024-02-29 15:39:56 +0300

    makechange

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,14 +7,14 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    least_Coin = [float('inf')] * (total + 1)
+    least_Coin[0] = 0
     for coin in coins:
         for amount in range(coin, total + 1):
-            if dp[amount - coin] + 1 < dp[amount]:
-                dp[amount] = dp[amount - coin] + 1
+            if least_Coin[amount - coin] + 1 < least_Coin[amount]:
+                least_Coin[amount] = least_Coin[amount - coin] + 1
 
-    if dp[total] == float('inf'):
+    if least_Coin[total] == float('inf'):
         return -1
     else:
-        return dp[total]
+        return least_Coin[total]
