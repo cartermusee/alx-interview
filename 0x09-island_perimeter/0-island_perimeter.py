79f43abc9146d2879321fc5f73f01a52375fd752
macilah-carter <museemacilah@gmail.com>
2024-03-08 09:56:20 +0300

    perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,6 +4,9 @@
 
 def island_perimeter(grid):
     """function which return perimeter of island"""
+    if not grid or not grid[0]:
+        return 0
+    rows, cols = len(grid), len(grid[0])
     per = 0
     for i in range(len(grid)):
         for j in range(len(grid)):
